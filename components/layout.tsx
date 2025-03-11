97122f54772faf6b889e34b26b9af280441d0b0f
natnaelshiferaw2123 <natnaelshiferaw2123@gmail.com>
2025-03-11 14:08:23 +0000

    add the layout component which consists the header and the footer components

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import type { ReactNode } from "react"
+import Header from "./header"
+import Footer from "./footer"
 
-export default function Layout() {
+interface LayoutProps {
+  children: ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div>Layout</div>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </div>
   )
 }
+
