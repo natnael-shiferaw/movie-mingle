7f019d93fa14a27f680bb43df9a789b900b3937c
Natnael Shiferaw <130279239+natnael-shiferaw@users.noreply.github.com>
2025-03-12 23:44:13 +0300

    Add comments for ThemeProvider component

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,7 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
+// Provides theme context using next-themes
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
